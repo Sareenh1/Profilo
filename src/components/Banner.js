cabbc9cc9afe383ca2836dfa166b3b5d4a1f20e2
H Sareen Kumar <127433171+Sareenh1@users.noreply.github.com>
2024-05-09 20:36:09 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">about my Portfolio</span>
                 <h1>{`Hi! I'm Sareen Kumar`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "DevOps Intern", "at InternCareer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Interncareer offering devops internships. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <p>Interncareer. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
